5aa52ccd669243f4aab5e20571eccebd233b1a4a
JesmarAmado <jandresamado42@gmail.com>
2021-09-30 22:20:03 +0000

    %zu and Sizeof

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char num1;
+	int num2;
+	long int num3;
+	long long int num4;
+	float num5;
+	printf("Size of a char:%zu byte(s)\n", sizeof(num1));
+	printf("Size of an int:%zu byte(s)\n", sizeof(num2));
+	printf("Size of a long int:%zu byte(s)\n", sizeof(num3));
+	printf("Size of a long long int:%zu byte(s)\n", sizeof(num4));
+	printf("Size of a float:%zu byte(s)\n", sizeof(num5));
+	return (0);
+}
