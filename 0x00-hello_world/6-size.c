67c0efa2d5b00514d295c8e0d94d1391b0033040
JesmarAmado <jandresamado42@gmail.com>
2021-09-30 22:41:14 +0000

    Sizeof(variable) -+-%zu

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,6 +11,7 @@ int main(void)
 	long int num3;
 	long long int num4;
 	float num5;
+
 	printf("Size of a char: %zu byte(s)\n", sizeof(num1));
 	printf("Size of an int: %zu byte(s)\n", sizeof(num2));
 	printf("Size of a long int: %zu byte(s)\n", sizeof(num3));
