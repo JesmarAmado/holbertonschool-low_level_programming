1cd640c0c7ef758cb1132445bb0a8e8f196075dd
JesmarAmado <jandresamado42@gmail.com>
2021-10-01 21:23:02 +0000

    Last_digit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char i = 'a';
+
+	while (i <= 'z')
+	{
+
+		putchar (i);
+		i++;
+	}
+	putchar (10);
+
+	return (0);
+}
