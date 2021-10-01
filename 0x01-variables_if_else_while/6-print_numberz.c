c9945e9fdb1854aa066bba2e24ed3694512838f9
JesmarAmado <jandresamado42@gmail.com>
2021-10-01 22:50:20 +0000

    Putchar_0-9

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int a = 48;
+
+	while (a <= 57)
+	{
+		putchar(a);
+		a++;
+	}
+
+	putchar(10);
+	return (0);
+}
