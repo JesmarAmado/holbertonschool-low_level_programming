2ad792a9220f6167685ff0564c60bda6c348bd68
JesmarAmado <jandresamado42@gmail.com>
2021-10-05 23:04:11 +0000

    _islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _islower - check the code.
+ *
+ * Return: 0
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	if (c >= 65 && c <= 90)
+	{
+		return (0);
+	}
+
+	return (0);
+}
