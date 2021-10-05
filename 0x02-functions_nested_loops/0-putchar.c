739ff8915f676175e5c809c0028df3ef976e1104
jandresamado@gmail.com <jandresamado@gmail.com>
2021-10-05 17:47:49 +0000

    Print-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,6 +7,23 @@
  */
 int main(void)
 {
-	printf("_putchar\n");
+	char *a = "_";
+	char *b = "p";
+	char *c = "u";
+	char *d = "t";
+	char *e = "c";
+	char *f = "h";
+	char *g = "a";
+	char *h = "r";
+
+	putchar(*a);
+	putchar(*b);
+	putchar(*c);
+	putchar(*d);
+	putchar(*e);
+	putchar(*f);
+	putchar(*g);
+	putchar(*h);
+	putchar(10);
 	return (0);
 }
