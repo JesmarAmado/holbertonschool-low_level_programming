9762ef2e411933b4f24884e89317ee4e4425cec6
jandresamado@gmail.com <jandresamado@gmail.com>
2021-10-07 15:22:51 +0000

    Print_0 to 9 but dont 2 and 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,5 +7,17 @@
  */
 void print_most_numbers(void)
 {
+	int a;
 
+	a = 0 + '0';
+
+	while (a <= 9 + '0')
+	{
+		if (a != (2 + '0') && a != (4 + '0'))
+		{
+			_putchar(a);
+		}
+		a++;
+	}
+	_putchar(10);
 }
