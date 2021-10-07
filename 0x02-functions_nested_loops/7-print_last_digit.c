0451f8b2a5a3d8374b1ad3709849ea92d2a52d26
JesmarAmado <jandresamado42@gmail.com>
2021-10-07 00:35:51 +0000

    LastDigit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,10 +3,24 @@
 /**
  * print_last_digit - Entry
  *
- * @n: variable
- * Return: n
+ * @d: variable
+ * Return: 4
  */
-int print_last_digit(int n)
+int print_last_digit(int d)
 {
-	return ((n % 10) + '0');
+	int u;
+	int a;
+
+	if (d >= 0)
+	{
+		u = d % 10;
+		_putchar(u + '0');
+	}
+	if (d < 0)
+	{
+		a = (d % 10) * -1;
+		_putchar(a + '0');
+	}
+
+	return (4);
 }
