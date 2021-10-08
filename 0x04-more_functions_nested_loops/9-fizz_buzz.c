fca9baf70474fc03e8360b8a662bcba482218e9c
JesmarAmado <jandresamado42@gmail.com>
2021-10-08 04:09:59 +0000

    FizzBuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int a;
+
+	a = 1;
+
+	while (a <= 100)
+	{
+		if (a % 5 == 0 || a % 3 == 0)
+		{
+			if (a % 3 == 0)
+			{
+				printf("Fizz");
+			}
+			if (a % 5 == 0)
+			{
+				printf("Buzz");
+			}
+		}
+		else
+		{
+			printf("%d", a);
+		}
+		if (a != 100)
+		{
+			putchar(' ');
+		}
+		a++;
+	}
+	
+	putchar(10);
+	return (0);
+}
