b1d92596c75e8812bcb9d762e1c8381a0bc0b0fc
JesmarAmado <jandresamado42@gmail.com>
2021-10-11 01:14:24 +0000

    DIGITS

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,61 @@
+#include "main.h"
+
+/**
+ * print_times_table - Entry
+ * @n: paremeter
+ * Return: 0
+ */
+void print_times_table(int n)
+{
+	int a;
+	int b;
+	int c;
+
+	if (n > 0 && n < 15)
+	{
+		for (a = 0 ; a <= n ; a++)
+		{
+			for (b = 0 ; b <= n ; b++)
+			{
+				c = a * b;
+
+				if (c < 10)
+				{
+					_putchar((c % 10) + '0');
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+				}
+				if (c >= 10)
+				{
+					if (c >= 100)
+					{
+						_putchar((c / 100) + '0');
+						_putchar(((c / 10) % 10) + '0');
+						_putchar((c % 10) + '0');
+						_putchar(',');
+						_putchar(' ');
+						_putchar(' ');
+					}
+					else 
+					{
+						_putchar((c / 10) + '0');
+						_putchar((c % 10) + '0');
+						_putchar(',');
+						_putchar(' ');
+						_putchar(' ');
+						_putchar(' ');
+					}
+				}
+			}
+			_putchar(10);
+		}
+	}
+
+	if (n <= 0 && n >= 15)
+	{
+		_putchar(10);
+	}
+}
