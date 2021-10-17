9449c26d6d6688af1da3079cf0979a44f8612cba
JesmarAmado <jandresamado42@gmail.com>
2021-10-17 19:13:49 +0000

    Of 2 in 2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,13 +9,26 @@
 void puts2(char *str)
 {
 	int a;
+	int b;
 
 	a = 0;
-
 	while (str[a])
 	{
-		_putchar(str[a]);
-		a += 2;
+		b = a + 1;
+		if (str[b] == '\0')
+		{
+			_putchar('.');
+			_putchar(10);
+			break;
+		}
+		else
+		{
+			_putchar(str[a]);
+			a += 2;
+		}
+		if (str[a] == '\0')
+		{
+			_putchar(10);
+		}
 	}
-	_putchar(10);
 }
