f5ab2a8b29d64b250c67226d0ebc8f9acca9e5bf
JesmarAmado <jandresamado42@gmail.com>
2021-10-17 17:38:28 +0000

    Print_rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,11 +9,18 @@
 void print_rev(char *s)
 {
 	int a;
+	int b;
 
-	a = 62;
+	a = 0;
 	while (s[a])
 	{
-		_putchar(s[a]);
+		a++;
+	}
+
+	b = a - 1;
+	while (s[b])
+	{
+		_putchar(s[b]);
 		s--;
 	}
 	_putchar(10);
