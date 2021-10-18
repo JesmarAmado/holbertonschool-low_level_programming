2c25c486f49fe546cdf2fd830e2a4eb88ea9f0f8
JesmarAmado <jandresamado42@gmail.com>
2021-10-18 00:44:22 +0000

    Puts2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,26 +9,16 @@
 void puts2(char *str)
 {
 	int a;
-	int b;
 
 	a = 0;
 	while (str[a])
 	{
-		b = a + 1;
-		if (str[b] == '\0')
+		_putchar(str[a]);
+		a += 2;
+		if (str[a - 1] == '\0')
 		{
-			_putchar('.');
-			_putchar(10);
 			break;
 		}
-		else
-		{
-			_putchar(str[a]);
-			a += 2;
-		}
-		if (str[a] == '\0')
-		{
-			_putchar(10);
-		}
 	}
+	_putchar(10);
 }
